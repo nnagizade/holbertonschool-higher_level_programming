d0a042af257b45a6b9a77328050516f3d8af3f3a
nnagizade <nagizadenigar05@gmail.com>
2026-03-26 19:13:17 +0000

    task 2 finished

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("{} arguments.".format(count))
+    elif count == 1:
+        print("{} argument:".format(count))
+        print("{}: {}".format(count, sys.argv[1]))
+    else:
+        print("{} arguments:".format(count))
+        for i in range(1, count + 1):
+            print("{}: {}".format(i, sys.argv[i]))
+
