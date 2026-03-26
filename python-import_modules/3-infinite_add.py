d66eb1205baf4ae03c0736e4dcda33000903015d
nnagizade <nagizadenigar05@gmail.com>
2026-03-26 19:40:00 +0000

    fixed 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 import sys
 if __name__ == "__main__":
     count = len(sys.argv) - 1
-    total = 0 
+    total = 0
     for i in range(1, count + 1):
         total = total + int(sys.argv[i])
     print("{}".format(total))
