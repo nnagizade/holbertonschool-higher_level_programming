8c2dec9e9be04a2f298703fe873b3858235040ea
nnagizade <nagizadenigar05@gmail.com>
2026-03-26 19:35:51 +0000

    task 3 finished

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    count = len(sys.argv) - 1
+    total = 0 
+    for i in range(1, count + 1):
+        total = total + int(sys.argv[i])
+    print("{}".format(total))
