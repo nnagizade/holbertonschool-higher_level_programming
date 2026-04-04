e381dd604759a6d6511757bad902aa176cb7b102
nnagizade <nagizadenigar05@gmail.com>
2026-04-04 11:20:51 +0000

    vf

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -62,6 +62,24 @@ class Rectangle:
             return 0
         return (self.__width * 2) + (self.__height * 2)
 
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Returns the Rectangle with the greater area.
+
+        Args:
+            rect_1 (Rectangle): The first Rectangle.
+            rect_2 (Rectangle): The second Rectangle.
+        Raises:
+            TypeError: If either rect_1 or rect_2 is not a Rectangle.
+        """
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
+
     def __str__(self):
         """Returns a printable representation of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
