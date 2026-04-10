8e8ee2b034ffa44b59cc6c4dd75055a253c9a405
nnagizade <nagizadenigar05@gmail.com>
2026-04-10 16:54:02 +0000

    Fix Task 3: Simplified CountedIterator

--- python-abc/task_03_counted_iterator.py
+++ python-abc/task_03_counted_iterator.py
@@ -7,11 +7,11 @@ Module for CountedIterator class.
 class CountedIterator:
     """An iterator that keeps track of the number of items iterated over."""
 
-    def __init__(self, iterable):
+    def __init__(self, data):
         """
         Initialize the iterator object and a counter.
         """
-        self.iterator = iter(iterable)
+        self.iterator = iter(data)
         self.counter = 0
 
     def get_count(self):
@@ -21,15 +21,11 @@ class CountedIterator:
     def __next__(self):
         """
         Increments the counter and fetches the next item.
-        Raises StopIteration if no items are left.
         """
-        try:
-            item = next(self.iterator)
-            self.counter += 1
-            return item
-        except StopIteration:
-            raise StopIteration
+        item = next(self.iterator)
+        self.counter += 1
+        return item
 
     def __iter__(self):
-        """Returns the iterator object itself."""
+        """Returns itself to support iteration."""
         return self
