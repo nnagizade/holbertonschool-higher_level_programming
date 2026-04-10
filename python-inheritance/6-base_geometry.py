adba0885a91cd48317ecca30fd77d0ace857d8ca
nnagizade <nagizadenigar05@gmail.com>
2026-04-10 15:51:16 +0000

    bsgm

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module contains the class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    A class with a public instance method area.
+    """
+
+    def area(self):
+        """
+        Raises an Exception with a specific message.
+        """
+        raise Exception("area() is not implemented")
